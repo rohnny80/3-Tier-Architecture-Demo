6fbd3bbb185d68417eaf34dbef412d9efd707f0e
rohnny80 <93962254+rohnny80@users.noreply.github.com>
2024-06-21 23:19:09 -0500

    fix perms

---
+++ fluentd/Docker-Compose/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+IMAGE_NAME="robotshop/fluentd:elastic"
+
+docker run \
+    -d \
+    --rm \
+    --name fluentd \
+    -p 24224:24224 \
+    -v $(pwd)/fluent.conf:/fluentd/etc/fluent.conf \
+    $IMAGE_NAME
